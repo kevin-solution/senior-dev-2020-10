fea9a5520397d1d8967c0d86a75658d5aad2c574
kevin-solution <kevin.solution41@gmail.com>
2020-10-24 13:30:59 +0000

    Add get repos endpoint

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,31 @@
 import { Router, Request, Response } from 'express';
+import { httpClient } from '../utils/axios';
+import { Repo } from '../typings/Repo';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
+  res.header('Content-Type', 'application/json');
 
-  res.status(200);
+  try {
+    const response = await httpClient.get<Repo[]>({
+      url: '/repos'
+    });
 
-  // TODO: See README.md Task (A). Return repo data here.
-  res.json([]);
+    const nonForkedRepos = response.filter(repo => !repo.fork);
+    console.log('resp', nonForkedRepos);
+
+    res.status(200);
+    res.json({
+      success: true,
+      data: nonForkedRepos,
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error'
+    });
+  }
 });
